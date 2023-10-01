4df784a8fecae9bb553bc2f3c338b798573f7fa3
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2023-10-01 17:11:38 -0300

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -10,11 +10,11 @@ from implementations and allowing different parts of a program to work together 
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
-void add(int a, int b);
-void substract(int a, int b);
-void mult(int a, int b);
-void divide(int a, int b);
-int calculator();
+void add ( int a , int b ) ;
+void substract ( int a, int b ) ;
+void mult ( int a, int b ) ;
+void divide ( int a, int b ) ;
+int calculator ( ) ;
 
 #endif
 
