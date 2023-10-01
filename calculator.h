69530d17e44f42a37795f8c66b05d4e29020f415
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2023-10-01 16:49:42 -0300

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -1,3 +1,12 @@
+/*
+A .h file, also known as a header file, contains function prototypes and declarations. 
+It serves as an interface for other source files to access the functions defined in a 
+corresponding .cpp or .c file with the same name. Including a header file in source files is 
+crucial for code organization and modularity, enabling the separation of function declarations
+from implementations and allowing different parts of a program to work together seamlessly and efficiently.
+*/
+
+
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
