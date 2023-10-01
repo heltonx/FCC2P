7a3e38125e850935d3d1759e2cafdcae58d71013
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2023-10-01 17:11:53 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //this programa has saveral funcions. A little legend with three heads. Probably I'm mixing C and C++
 
 #include <iostream>
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
 #include "calculator.h"
 
